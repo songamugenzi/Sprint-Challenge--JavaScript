f88e6f5ff79e93e4f77fd87e0771a249c1966e84
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2020-04-24 13:59:55 -0400

    Finished the closure section.

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,15 @@ myFunction();
 
 // Explanation: 
 
+//'nestedFunction' is able to access the 'internal' variable because it's within its lexical scope.
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+const sumation = function (addThis) {
+  let counter = addThis;
+  counter += addThis - 1;
+
+  return counter
+}
+console.log(sumation(4));
