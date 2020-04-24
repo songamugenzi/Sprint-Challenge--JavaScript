cd40770e84c88fb1e6616e9870b2fa7e3c771844
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2020-04-24 13:03:15 -0400

    Finished Advanced arrays section

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -21,6 +21,9 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach(function (item) {
+  return displayNames.push(`Name: ${item.animal_name}, Scientific: ${item.scientific_name}`)
+});
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -29,7 +32,9 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map(function (item) {
+  return item.animal_name.toLowerCase()
+});
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -37,7 +42,10 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+
+const lowPopulationAnimals = zooAnimals.filter(function (item) {
+  return item.population < 5;
+});
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -45,7 +53,10 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+
+const populationTotal = zooAnimals.reduce(function (accumulator, currentValue) {
+  return accumulator + currentValue.population;
+}, 0);
 console.log(populationTotal);
 
 
@@ -59,9 +70,10 @@ console.log(populationTotal);
 */
 
 
+
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
+  * Create a function named multiply that returns the product of two numbers
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
